173b06cd5ebd2ac50f783e338b60b304553a348d
by46 <ycs_ctbu_2010@126.com>
2017-08-11 17:29:58 +0800

    feat: fix bug

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    Main Page
+    <p>{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
   </div>
 </template>
 
 <script>
+  import Vuex from 'vuex'
+
+  const store = new Vuex.Store({
+    state: {
+      count: 0
+    },
+    mutations: {
+      increment: state => state.count++,
+      decrement: state => state.count--
+    }
+  })
   export default {
     name: 'hello',
     data () {
@@ -28,7 +28,20 @@
         msg: 'Welcome to Your Vue.js App'
       }
     },
-    components: {}
+    components: {},
+    computed: {
+      count () {
+        return store.state.count
+      }
+    },
+    methods: {
+      increment () {
+        store.commit('increment')
+      },
+      decrement () {
+        store.commit('decrement')
+      }
+    }
   }
 </script>
 
